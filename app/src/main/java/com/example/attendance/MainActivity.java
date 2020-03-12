2c8ad0b182cbd0478283bb29f3da53f90db5eeeb
Md Nazmus Sakib <nzmsakib@users.noreply.github.com>
2020-03-12 10:11:12 +0600

    Merge branch 'master' of https://github.com/nzmsakib/Attendance into begin
    # Conflicts:
    #	app/src/main/java/com/example/attendance/MainActivity.java

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -1,17 +1,47 @@
 package com.example.attendance;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialTextInputPicker;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //UpdatedbySakib;
-        //Hi there.
-        // I'm editable
+
+        final TextInputLayout til = findViewById(R.id.textInputLayout);
+        final TextInputEditText tie = findViewById(R.id.tied);
+
+
+        tie.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    String s;
+                    s = tie.getText().toString();
+                    if(!s.isEmpty()&&!s.contains("@")){
+                        til.setError("Invalid UserID.!");
+                    }
+                    else
+                        til.setErrorEnabled(false);
+                }
+                else
+                    til.setHelperText("Enter your UserID.");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/attendance/db/fetch.java
@@ -0,0 +1,10 @@
+package com.example.attendance.db;
+
+import android.os.AsyncTask;
+
+public class fetch extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
